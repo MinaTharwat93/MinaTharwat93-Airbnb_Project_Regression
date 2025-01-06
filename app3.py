f5502a9e71a497c05ea98f7bdcc9d82de99c5772
MinaTharwat93 <166447605+MinaTharwat93@users.noreply.github.com>
2025-01-07 01:15:36 +0200

    Update app3.py

--- app3.py
+++ app3.py
@@ -64,7 +64,7 @@ def main():
         We use GridSearchCV to fine-tune the model and find the best hyperparameters.
     """)
 
-    st.image("D:\Epsilon Ai\Airbnb-Project\static\images/pipe1.png", caption="pipeline")
+    st.image("/mount/src/minatharwat93-airbnb_project_regression/static/images/pipe1.png", caption="pipeline")
 
     # Models and Grid Search
     st.subheader('Models & Grid Search')
@@ -74,8 +74,8 @@ def main():
         • Linear Regression:
         We use Linear Regression and observe its performance before and after hyperparameter tuning. Accuracy was low using polynomial features.
     """)
-    st.image("D:\Epsilon Ai\Airbnb-Project\static\images/lin1.png", caption="Linear Regression")
-    st.image("D:\Epsilon Ai\Airbnb-Project\static\images/lin 2.png", caption="Linear Regression_Poly")
+    st.image("/mount/src/minatharwat93-airbnb_project_regression/static/images/lin1.png", caption="Linear Regression")
+    st.image("/mount/src/minatharwat93-airbnb_project_regression/static/images/lin 2.png", caption="Linear Regression_Poly")
 
 
     # Lasso
@@ -84,8 +84,8 @@ def main():
         We apply Lasso Regression and tune it using GridSearchCV.
     """)
 
-    st.image("D:\Epsilon Ai\Airbnb-Project\static\images/la1.png", caption="Lasso")
-    st.image("D:\Epsilon Ai\Airbnb-Project\static\images/la2.png", caption="Lasso with gridsearch")
+    st.image("/mount/src/minatharwat93-airbnb_project_regression/static/images/la1.png", caption="Lasso")
+    st.image("/mount/src/minatharwat93-airbnb_project_regression/static/images/la2.png", caption="Lasso with gridsearch")
 
     # Ridge
     st.write("""
@@ -93,65 +93,65 @@ def main():
         Ridge Regression is tested and tuned using GridSearchCV.
     """)
 
-    st.image("D:\Epsilon Ai\Airbnb-Project\static\images/ri1.png", caption="Ridge")
-    st.image("D:\Epsilon Ai\Airbnb-Project\static\images/ri2.png", caption="Ridge with gridsearch")
+    st.image("/mount/src/minatharwat93-airbnb_project_regression/static/images/ri1.png", caption="Ridge")
+    st.image("/mount/src/minatharwat93-airbnb_project_regression/static/images/ri2.png", caption="Ridge with gridsearch")
 
     # ElasticNet
     st.write("""
         • ElasticNet:
         ElasticNet is tested and fine-tuned using GridSearchCV.
     """)
-    st.image("D:\Epsilon Ai\Airbnb-Project\static\images/el1.png", caption="ElasticNet")
-    st.image("D:\Epsilon Ai\Airbnb-Project\static\images/el2.png", caption="ElasticNet with gridsearch")
+    st.image("/mount/src/minatharwat93-airbnb_project_regression/static/images/el1.png", caption="ElasticNet")
+    st.image("/mount/src/minatharwat93-airbnb_project_regression/static/images/el2.png", caption="ElasticNet with gridsearch")
 
     # KNeighborsRegressor
     st.write("""
         • KNeighborsRegressor:
         K-Nearest Neighbors Regression is used and tuned with GridSearchCV.
     """)
-    st.image("D:\Epsilon Ai\Airbnb-Project\static\images/kn1.png", caption="KNeighborsRegressor")
-    st.image("D:\Epsilon Ai\Airbnb-Project\static\images/kn2.png", caption="KNeighborsRegressor with gridsearch")
+    st.image("/mount/src/minatharwat93-airbnb_project_regression/static/images/kn1.png", caption="KNeighborsRegressor")
+    st.image("/mount/src/minatharwat93-airbnb_project_regression/static/images/kn2.png", caption="KNeighborsRegressor with gridsearch")
 
     # DecisionTreeRegressor
     st.write("""
         • DecisionTreeRegressor:
         Decision Tree Regressor is tested and tuned using GridSearchCV.
     """)
-    st.image("D:\Epsilon Ai\Airbnb-Project\static\images/dt1.png", caption="DecisionTreeRegressor")
-    st.image("D:\Epsilon Ai\Airbnb-Project\static\images/dt2.png", caption="DecisionTreeRegressor with gridsearch")
+    st.image("/mount/src/minatharwat93-airbnb_project_regression/static/images/dt1.png", caption="DecisionTreeRegressor")
+    st.image("/mount/src/minatharwat93-airbnb_project_regression/static/images/dt2.png", caption="DecisionTreeRegressor with gridsearch")
 
     # RandomForestRegressor
     st.write("""
         • RandomForestRegressor:
         Random Forest Regressor is used and fine-tuned using GridSearchCV.
     """)
-    st.image("D:\Epsilon Ai\Airbnb-Project\static\images/rf1.png", caption="RandomForestRegressor")
-    st.image("D:\Epsilon Ai\Airbnb-Project\static\images/rf2.png", caption="RandomForestRegressor with gridsearch")
+    st.image("/mount/src/minatharwat93-airbnb_project_regression/static/images/rf1.png", caption="RandomForestRegressor")
+    st.image("/mount/src/minatharwat93-airbnb_project_regression/static/images/rf2.png", caption="RandomForestRegressor with gridsearch")
 
     # XGBRegressor
     st.write("""
         • XGBRegressor:
         XGBoost Regressor is applied and tuned.
     """)
-    st.image("D:\Epsilon Ai\Airbnb-Project\static\images/xg1.png", caption="XGBRegressor")
-    st.image("D:\Epsilon Ai\Airbnb-Project\static\images/xg2.png", caption="XGBRegressor with gridsearch")
+    st.image("/mount/src/minatharwat93-airbnb_project_regression/static/images/xg1.png", caption="XGBRegressor")
+    st.image("/mount/src/minatharwat93-airbnb_project_regression/static/images/xg2.png", caption="XGBRegressor with gridsearch")
 
     # GradientBoostingRegressor
     st.write("""
         • GradientBoostingRegressor:
         Gradient Boosting Regressor is tested and tuned using GridSearchCV with negative mean squared error. The best score will be negative, but the absolute value is considered.
     """)
-    st.image("D:\Epsilon Ai\Airbnb-Project\static\images/gb1.png", caption="GradientBoostingRegressor")
-    st.image("D:\Epsilon Ai\Airbnb-Project\static\images/pipe2.png", caption="pipeline with model")
-    st.image("D:\Epsilon Ai\Airbnb-Project\static\images/gb2.png", caption="GradientBoostingRegressor with gridsearch")
+    st.image("/mount/src/minatharwat93-airbnb_project_regression/static/images/gb1.png", caption="GradientBoostingRegressor")
+    st.image("/mount/src/minatharwat93-airbnb_project_regression/static/images/pipe2.png", caption="pipeline with model")
+    st.image("/mount/src/minatharwat93-airbnb_project_regression/static/images/gb2.png", caption="GradientBoostingRegressor with gridsearch")
 
     # Test Score & Confidence Interval
     st.write("""
         4) Test Score & Confidence Interval
         The final model’s performance is evaluated on the test set, and a confidence interval is calculated to assess the stability of the model.
     """)
-    st.image("D:\Epsilon Ai\Airbnb-Project\static\images/ggb1.png", caption=" train & test GradientBoostingRegressor with gridsearch")
-    st.image("D:\Epsilon Ai\Airbnb-Project\static\images/ggb2.png", caption="train & test GradientBoostingRegressor")
+    st.image("/mount/src/minatharwat93-airbnb_project_regression/static/images/ggb1.png", caption=" train & test GradientBoostingRegressor with gridsearch")
+    st.image("/mount/src/minatharwat93-airbnb_project_regression/static/images/ggb2.png", caption="train & test GradientBoostingRegressor")
 
     # Save Model
     st.write("""
